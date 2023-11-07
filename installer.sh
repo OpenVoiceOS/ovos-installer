0b6a21a7ec872e9595229244a1eb0935055119ee
Gaëtan Trellu <gaetan.trellu@gmail.com>
2023-11-07 15:37:19 -0500

    [installer.sh] Update repository url

--- installer.sh
+++ installer.sh
@@ -6,6 +6,6 @@ if [[ -d "$installer_path" ]]; then
     rm -rf "$installer_path"
 fi
 
-git clone --quiet https://github.com/OpenVoiceOS/ovos-installer.git "$installer_path"
+git clone --quiet https://github.com/smartgic/ovos-installer.git "$installer_path"
 cd "$installer_path" || exit 1
 bash ./setup.sh
