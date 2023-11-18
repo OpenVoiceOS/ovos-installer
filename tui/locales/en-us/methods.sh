46945b1e674d62207cd9e1deed614332a74d71e7
Gaëtan Trellu <gaetan.trellu@gmail.com>
2023-11-18 18:47:21 -0500

    [tui] Mention docker installation if not present

--- tui/locales/en-us/methods.sh
+++ tui/locales/en-us/methods.sh
@@ -8,6 +8,8 @@ To install Open Voice OS, you have two primary methods:
 
 Containers provide isolation and easy deployment, while a Python virtual environment offers more flexibility and control over the installation.
 
+If the containers method is selected, Docker will be installed automatically if not present on the system.
+
 Please select an installation method:
 "
 TITLE="Open Voice OS Installation - Methods"
--- tui/locales/fr-fr/methods.sh
+++ tui/locales/fr-fr/methods.sh
@@ -8,6 +8,8 @@ Il existe deux méthodes pour installation Open Voice OS:
 
 Les conteneurs offrent une isolation et un déploiement facile, tandis qu'un environnement virtuel Python offre plus de flexibilité et de contrôle sur l'installation.
 
+Si la méthode conteneurs est sélectionnée, Docker sera installé automatiquement s'il n'est pas présent sur le système.
+
 Veuillez sélectionner une méthode d'installation:
 "
 TITLE="Open Voice OS Installation - Méthodes d'installation"
