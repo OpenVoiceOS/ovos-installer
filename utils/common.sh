1f129bbd2bb9c50a5dd32a5c064ff13d64acc18e
Gaëtan Trellu <gaetan.trellu@gmail.com>
2023-11-21 15:55:43 -0500

    [common] Repalce python3-venv by python3.11-venv

--- utils/common.sh
+++ utils/common.sh
@@ -187,7 +187,7 @@ function required_packages() {
     case $DISTRO_NAME in
     debian | ubuntu)
         apt-get update &>>"$LOG_FILE"
-        apt-get install --no-install-recommends -y python3.11 python3.11-dev python3-pip python3-venv whiptail expect jq &>>"$LOG_FILE"
+        apt-get install --no-install-recommends -y python3.11 python3.11-dev python3-pip python3.11-venv whiptail expect jq &>>"$LOG_FILE"
         ;;
     fedora)
         dnf install -y python3.11 python3.11-devel python3-pip python3-virtualenv newt expect jq &>>"$LOG_FILE"
