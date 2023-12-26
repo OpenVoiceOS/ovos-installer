c7e6618a7e1651fa4685741c92ce96882246d5f5
timonvanhasselt <timonvanhasselt@users.noreply.github.com>
2023-12-26 20:29:46 +0100

    Update tuning.sh

--- tui/locales/nl-nl/tuning.sh
+++ tui/locales/nl-nl/tuning.sh
@@ -3,9 +3,9 @@
 CONTENT="
 Open Voice OS Tuning for Raspberry Pi optimaliseert het besturingssysteem zodat het efficiënt draait op de Raspberry Pi hardware.
 
-Dit tuningproces heeft als doel de prestaties te verbeteren, het gebruik van bronnen te verminderen en een soepelere gebruikerservaring te garanderen, waardoor het een uitstekende keuze is voor apparaten met beperkte bronnen zoals de Raspberry Pi.
+Dit tuningproces heeft als doel de prestaties te verbeteren, het gebruik van bronnen te verminderen en een soepelere gebruikerservaring te garanderen, waardoor het een uitstekende keuze is voor apparaten met beperkte rekenkracht zoals de Raspberry Pi.
 
-Tuning inschakelen voor Raspberry Pi (aanbevolen)?
+'Tuning' inschakelen voor Raspberry Pi? (aanbevolen)
 "
 TITLE="Open Voice OS Installation - Tuning"
 
