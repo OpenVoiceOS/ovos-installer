0008ec968d1c2879cdf3860c71f309fba0598766
Gaëtan Trellu <gaetan.trellu@gmail.com>
2024-03-25 21:54:29 -0400

    [mark2] Add locales

--- tui/locales/nl-nl/detection.sh
+++ tui/locales/nl-nl/detection.sh
@@ -3,14 +3,15 @@
 CONTENT="
 Automatisch herkende systeemeigenschappen:
 
-    - OS:       ${DISTRO_NAME^} $DISTRO_VERSION
-    - Kernel:   $KERNEL
-    - RPi:      $RASPBERRYPI_MODEL
-    - Python:   $(echo "$PYTHON" | awk '{ print $NF }')
-    - AVX/SIMD: $CPU_IS_CAPABLE
-    - Venv:     $VENV_PATH
-    - Geluid:    $SOUND_SERVER
-    - Beeldscherm:  ${DISPLAY_SERVER^}
+    - OS:                 ${DISTRO_NAME^} $DISTRO_VERSION
+    - Kernel:             $KERNEL
+    - RPi:                $RASPBERRYPI_MODEL
+    - Python:             $(echo "$PYTHON" | awk '{ print $NF }')
+    - AVX/SIMD:           $CPU_IS_CAPABLE
+    - sprzęt komputerowy: $HARDWARE_DETECTED
+    - Venv:               $VENV_PATH
+    - Geluid:             $SOUND_SERVER
+    - Beeldscherm:        ${DISPLAY_SERVER^}
 "
 TITLE="Open Voice OS Installatie - Systeemeigenschappen"
 
