278020380e3a2b2e065bdd63978ff0a0ade329c0
Vaibhav-Magadum <vaibhavmagadum859@gmail.com>
2024-04-17 11:04:37 +0530

    // Hindi language support added

---
+++ tui/locales/hi-in/detection.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+CONTENT="
+कृपया पता लगाई गई जानकारी को सत्यापित करें:
+
+    - OS:       ${DISTRO_NAME^} $DISTRO_VERSION
+    - Kernel:   $KERNEL
+    - RPi:      $RASPBERRYPI_MODEL
+    - Python:   $(echo "$PYTHON" | awk '{ print $NF }')
+    - AVX/SIMD: $CPU_IS_CAPABLE
+    - Hardware: $HARDWARE_DETECTED
+    - Venv:     $VENV_PATH
+    - Sound:    $SOUND_SERVER
+    - Display:  ${DISPLAY_SERVER^}
+"
+TITLE="Open Voice OS Installation - सिस्टम जानकारी की पहचान"
+
+export CONTENT TITLE
---
+++ tui/locales/hi-in/methods.sh
@@ -0,0 +1,17 @@
+#!/bin/env bash
+
+CONTENT="
+Open Voice OS इंस्टालेशन (install) करने के लिए, आपके पास दो प्राथमिक विधियाँ हैं:
+
+    - Containers engine such as Docker
+    - Setting it up in a Python virtual environment
+
+Containers (कंटेनर) अलगाव और आसान तैनाती प्रदान करते हैं, जबकि पायथन वर्चुअल वातावरण (Python virtual environment) इंस्टॉलेशन पर अधिक लचीलापन और नियंत्रण प्रदान करता है।
+
+यदि Container (कंटेनर) विधि का चयन किया गया है, सिस्टम पर मौजूद नहीं होने पर Docker(डॉकर) खुद ब खुद से इंस्टॉल हो जाएगा।
+
+कृपया एक इंस्टालेशन (install) विधि चुनें:
+"
+TITLE="Open Voice OS Installation - विधि"
+
+export CONTENT TITLE
