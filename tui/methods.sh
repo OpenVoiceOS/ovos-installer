fd624ffbdd9b35bf039a6d9ccdb89456c20c2228
Gaëtan Trellu <gaetan.trellu@gmail.com>
2024-05-02 11:55:33 -0400

    [mark1] Update comment

--- tui/methods.sh
+++ tui/methods.sh
@@ -9,7 +9,8 @@ available_methods=(containers virtualenv)
 
 # When 32-bit CPU is detected, the only method available
 # will be Python virtualenv as there are no 32-bit container
-# images available.
+# images available. Same for Raspberry Pi 3 as containers
+# might be too heavy for this board.
 if [[ "$ARCH" != @(x86_64|aarch64) ]] && [[ "$RASPBERRYPI_MODEL" == *"Raspberry Pi 3"* ]]; then
   active_method="virtualenv"
   available_methods=(virtualenv)
