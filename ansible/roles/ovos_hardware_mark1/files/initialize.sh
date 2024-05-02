cc1cdc1aef896c6a9519bdbdd2faa343a73b0457
Gaëtan Trellu <gaetan.trellu@gmail.com>
2024-05-02 12:39:01 -0400

    [mark1] Don't override volume set by user

--- ansible/roles/ovos_hardware_mark1/files/initialize.sh
+++ ansible/roles/ovos_hardware_mark1/files/initialize.sh
@@ -11,6 +11,7 @@ eyes_color="16760576"
 mouth_text="booting"
 tty_device=/dev/ttyAMA0
 alsa_card="sndrpiproto"
+alsa_configured=/opt/mark1/alsa.configured
 
 # Initialiaze the firmware and wait two seconds
 avrdude -p atmega328p -c linuxgpio -U signature:r:-:i -F
@@ -24,7 +25,11 @@ echo "mouth.text=$mouth_text" > "$tty_device"
 
 # Set default values to sndrpiproto ALSA card
 if grep "$alsa_card" /proc/asound/cards -q; then
-    amixer -c "$alsa_card" cset numid=1 100,100
+    # Set volume mixer only once
+    if [ ! -f "$alsa_configured" ]; then
+        amixer -c "$alsa_card" cset numid=1 100,100
+        touch "$alsa_configured"
+    fi
     amixer -c "$alsa_card" cset numid=2 on
     amixer -c "$alsa_card" cset numid=6 on
     amixer -c "$alsa_card" cset numid=10 on
