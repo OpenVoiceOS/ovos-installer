178cc55567e5937b128de95725beeec8d0069e7d
Gaëtan Trellu <gaetan.trellu@gmail.com>
2024-05-02 12:32:03 -0400

    [mark1] Configure sndrpiproto ALSA card

--- ansible/roles/ovos_hardware_mark1/files/initialize.sh
+++ ansible/roles/ovos_hardware_mark1/files/initialize.sh
@@ -9,6 +9,7 @@
 eyes_color="16760576"
 mouth_text="booting"
 tty_device=/dev/ttyAMA0
+alsa_card="sndrpiproto"
 
 # Initialiaze the firmware and wait two seconds
 avrdude -p atmega328p -c linuxgpio -U signature:r:-:i -F
@@ -19,3 +20,14 @@ echo "eyes.color=$eyes_color" > "$tty_device"
 
 # Set mouth text
 echo "mouth.text=$mouth_text" > "$tty_device"
+
+# Set default values to sndrpiproto ALSA card
+if grep "$alsa_card" /proc/asound/cards -q; then
+    amixer -c "$alsa_card" cset numid=1 100,100
+    amixer -c "$alsa_card" cset numid=2 on
+    amixer -c "$alsa_card" cset numid=6 on
+    amixer -c "$alsa_card" cset numid=10 on
+    amixer -c "$alsa_card" cset numid=14 1
+    amixer -c "$alsa_card" cset numid=13 on
+    amixer -c "$alsa_card" cset numid=9 on
+fi
