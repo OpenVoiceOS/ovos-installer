9d9da02d22bfe1a5f96a8a47f6ee65036b77e041
goldyfruit <gaetan.trellu@gmail.com>
2024-06-24 10:25:12 -0400

    [utils/common] Add no-recommends to zypper install

--- utils/common.sh
+++ utils/common.sh
@@ -217,7 +217,7 @@ function required_packages() {
         dnf install -y python3 python3-devel python3-pip newt expect jq "${extra_packages[@]}" &>>"$LOG_FILE"
         ;;
     opensuse-tumbleweed | opensuse-leap)
-        zypper install -y python3 python3-devel python3-pip python3-rpm newt expect jq "${extra_packages[@]}" &>>"$LOG_FILE"
+        zypper install --no-recommends -y python3 python3-devel python3-pip python3-rpm newt expect jq "${extra_packages[@]}" &>>"$LOG_FILE"
         ;;
     arch | manjaro | endeavouros)
         pacman -Sy --noconfirm python python-pip python-virtualenv libnewt expect jq "${extra_packages[@]}" &>>"$LOG_FILE"
