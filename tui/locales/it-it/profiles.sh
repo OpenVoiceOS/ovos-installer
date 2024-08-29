e60b369ed03ee6062b2e52dfc5312ee2eabccac9
Michael Moroni <michaelmoroni@disroot.org>
2024-08-29 10:08:47 +0200

    Update profiles.sh
    Fixed translation

--- tui/locales/it-it/profiles.sh
+++ tui/locales/it-it/profiles.sh
@@ -1,19 +1,19 @@
 #!/bin/env bash
 
 CONTENT="
-Per creare un sistema versatile e collegato in rete, adatto a molte applicazioni, esistono tre affascinanti opzioni:
+Per creare un sistema versatile e collegato alla rete, adatto a molte applicazioni, esistono tre affascinanti opzioni:
 
-  - Open Voice OS Master: la classica esperienza Open Voice OS
-  - HiveMind Satellite: Apri i componenti audio Voice OS collegati a HiveMind Listener
-  - HiveMind Listener: Hub per HiveMind Satellite, bot, ecc. a cui connettersi
-  - Server: Solo Open Voice OS Core e HiveMind Core, nessuna funzione audio
+  - Open Voice OS Master: la classica esperienza di Open Voice OS
+  - HiveMind Satellite: le componenti audio di Open Voice OS vengono collegate a HiveMind Listener
+  - HiveMind Listener: hub per HiveMind Satellite, bots, ecc a cui connettersi
+  - Server: solo Open Voice OS Core e HiveMind Core, nessuna funzione audio
 
 Ognuno di questi profili offre caratteristiche e funzionalità uniche che possono migliorare significativamente la vostra esperienza digitale.
 
 L'unica domanda da porsi è quale sia il profilo più adatto alle proprie esigenze e preferenze.
 
-Selezionate un profilo:
+Seleziona un profilo:
 "
-TITLE="Open Voice OS Installation - Profilo"
+TITLE="Installazione di Open Voice OS - Profilo"
 
 export CONTENT TITLE
