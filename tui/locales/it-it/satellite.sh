21f171e5cd2d81f007e92206bb5dad95c085ee2e
Michael Moroni <michaelmoroni@disroot.org>
2024-08-31 08:43:07 +0200

    Update satellite.sh
    Updated translation

--- tui/locales/it-it/satellite.sh
+++ tui/locales/it-it/satellite.sh
@@ -1,20 +1,20 @@
 #!/bin/env bash
 
 # Global message
-content="Collegandosi all'HiveMind Listener, i satelliti HiveMind accedono a una rete di conoscenze e competenze condivise che consente un'assistenza linguistica e un'automazione unificate ed efficienti."
+content="Collegandosi all'ascoltatore di HiveMind, i satelliti di HiveMind accedono a una rete di conoscenze e competenze condivise che consente un'assistenza linguistica e un'automazione unificate ed efficienti."
 
 # Host
 CONTENT_HOST="
 $content
 
-Inserire l'host dell'ascoltatore HiveMind (DNS o indirizzo IP):
+Inserisci l'host dell'ascoltatore di HiveMind (DNS o indirizzo IP):
 "
 
 # Port
 CONTENT_PORT="
 $content
 
-Inserire la porta dell'ascoltatore HiveMind:
+Inserire la porta dell'ascoltatore di HiveMind:
 "
 
 # Key
@@ -28,12 +28,12 @@ Immettere la chiave di ascolto HiveMind del satellite:
 CONTENT_PASSWORD="
 $content
 
-Inserire la password dell'ascoltatore HiveMind del satellite:
+Inserire la password dell'ascoltatore di HiveMind del satellite:
 "
 
-TITLE_HOST="Open Voice OS Installation - Satellite 1/4"
-TITLE_PORT="Open Voice OS Installation - Satellite 2/4"
-TITLE_KEY="Open Voice OS Installation - Satellite 3/4"
-TITLE_PASSWORD="Open Voice OS Installation - Satellite 4/4"
+TITLE_HOST="Installazione di Open Voice OS - Satellite 1/4"
+TITLE_PORT="Installazione di Open Voice OS - Satellite 2/4"
+TITLE_KEY="Installazione di Open Voice OS - Satellite 3/4"
+TITLE_PASSWORD="Installazione di Open Voice OS - Satellite 4/4"
 
 export CONTENT_HOST CONTENT_PORT CONTENT_KEY CONTENT_PASSWORD TITLE_HOST TITLE_PORT TITLE_KEY TITLE_PASSWORD
