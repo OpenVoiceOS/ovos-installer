470ecbdd9e84f0ae425a4119aaf71032131f7da3
Michael Moroni <michaelmoroni@disroot.org>
2024-08-31 09:13:33 +0200

    Update tuning.sh
    fix transl

--- tui/locales/it-it/tuning.sh
+++ tui/locales/it-it/tuning.sh
@@ -1,12 +1,12 @@
 #!/bin/env bash
 
 CONTENT="
-Open Voice OS Tuning for Raspberry Pi ottimizza il sistema operativo per farlo funzionare in modo efficiente sull'hardware del Raspberry Pi.
+L'ottimizzazione di Open Voice OS per Raspberry Pi ottimizza il sistema operativo per farlo funzionare in modo efficiente sull'hardware del Raspberry Pi.
 
 Questo processo di ottimizzazione mira a migliorare le prestazioni, ridurre l'utilizzo delle risorse e garantire un'esperienza utente più fluida, rendendolo una scelta eccellente per i dispositivi con risorse limitate come il Raspberry Pi.
 
-Attivare il tuning per Raspberry Pi (consigliato)?
+Attivi l'ottimizzazione per Raspberry Pi (consigliato)?
 "
-TITLE="Open Voice OS Installation - Ottimizzazione"
+TITLE="Installazione Open Voice OS - Ottimizzazione"
 
 export CONTENT TITLE
