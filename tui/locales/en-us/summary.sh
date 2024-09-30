266d6a06bb76c12aa064fda6dfa5443859d72b4f
miro <jarbasai@mailfence.com>
2024-09-30 20:51:05 +0100

    sync coderabbit suggestions

--- tui/locales/en-us/summary.sh
+++ tui/locales/en-us/summary.sh
@@ -7,12 +7,12 @@ You are almost done, here is a summary of choices you made to install Open Voice
     - Version:  $CHANNEL
     - Profile:  $PROFILE
     - GUI:      $FEATURE_GUI
-    - Skills    $FEATURE_SKILLS
+    - Skills:   $FEATURE_SKILLS
     - Tuning:   $TUNING
 
 The choices made during the Open Voice OS installation process have been carefully considered to tailor our system to your unique needs and preferences.
 
-Does is sound correct to you?
+Does this summary look correct to you? If not, you can go back and make changes.
 "
 TITLE="Open Voice OS Installation - Summary"
 
