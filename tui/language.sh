02bcb61b27dd543891cead0569711ab2f90ec35f
goldyfruit <gaetan.trellu@gmail.com>
2024-10-04 12:33:05 -0400

    [tui] Add Catalan language support

--- tui/language.sh
+++ tui/language.sh
@@ -5,7 +5,7 @@ Please select a language:
 "
 
 active_language="English"
-available_languages=(Dutch English French German Hindi Italian Portuguese Spanish)
+available_languages=(Catalan Dutch English French German Hindi Italian Portuguese Spanish)
 
 whiptail_args=(
   --title "Open Voice OS Installation - Language"
