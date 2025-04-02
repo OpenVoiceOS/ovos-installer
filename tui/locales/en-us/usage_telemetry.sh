1369030defd8274037b29b252f70d0b2871d3e51
goldyfruit <goldyfruit@users.noreply.github.com>
2025-04-02 17:45:43 +0000

    Update translations

---
+++ tui/locales/en-us/usage_telemetry.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+CONTENT="
+By opting to share your usage metrics with us, you play a crucial role in contributing valuable, anonymous information that enables us to better understand user experiences and improve our software's performance.
+
+Share data for refining intent matching and identifying system weaknesses. Self-host or contribute to official servers with a focus on transparency and privacy. Real-time insights available through an interactive dashboard. Benefits of self-hosting include analyzing personal usage trends, tracking failed intents, and fine-tuning the assistant.
+
+Please visit https://opendata.tigregotico.pt to visualize and/or download the data.
+
+Do you accept to share anonymous data with us?
+"
+TITLE="Open Voice OS Installation - Usage Metrics"
+
+export CONTENT TITLE
