7ab4ade9fa12b32db0002f1c930cdb4379252d1b
goldyfruit <goldyfruit@users.noreply.github.com>
2025-04-02 18:42:59 +0000

    Update translations

---
+++ tui/locales/fr-fr/usage_telemetry.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+CONTENT="
+En choisissant de partager vos indicateurs d'utilisation, vous contribuez activement à nos efforts d'amélioration continue des performances et de l'expérience utilisateur de notre logiciel. Les données que vous nous fournissez resteront anonymes et sont essentielles pour nous aider à affiner la correspondance des intentions, à identifier les faiblesses du système et à prendre des décisions éclairées concernant les développements futurs.
+
+En échange de votre participation, nous vous offrons des informations précieuses sur les tendances d'utilisation, les intentions échouées et des analyses personnalisées via un tableau de bord interactif. Ces informations peuvent vous aider à optimiser l'assistant pour qu'il réponde mieux à vos besoins spécifiques, garantissant ainsi une expérience utilisateur plus agréable et efficace.
+
+Pour plus d'informations sur la visualisation des données et les options de téléchargement, veuillez consulter notre portail Open Data : https://opendata.tigregotico.pt
+
+Votre contribution est importante et nous espérons que vous nous rejoindrez pour créer une meilleure expérience logicielle pour tous !
+
+Acceptez-vous de partager des données anonymes avec nous ?
+"
+TITLE="Open Voice OS Installation - Statistiques d'utilisation"
+
+export CONTENT TITLE
