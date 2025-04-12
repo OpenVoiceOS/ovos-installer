81b34665158970f008e67d9d4fbb3a00d0381db7
goldyfruit <goldyfruit@users.noreply.github.com>
2025-04-12 13:04:36 +0000

    Update translations

--- tui/locales/it-it/usage_telemetry.sh
+++ tui/locales/it-it/usage_telemetry.sh
@@ -5,12 +5,11 @@ Scegliendo di condividere i dati relativi al tuo utilizzo, contribuirai in modo 
 
 In cambio della tua partecipazione, ti offriremo informazioni preziose sulle tendenze di utilizzo, le intenzioni non riconosciute e analisi personalizzate tramite un dashboard interattivo. Queste informazioni possono aiutarti a ottimizzare l'assistente per adattarlo meglio alle tue esigenze specifiche, garantendo un’esperienza utente più piacevole ed efficiente.
 
-Per maggiori informazioni sulla visualizzazione dei dati e sulle opzioni di download, visita il nostro portale Open Data: 
-https://opendata.tigregotico.pt
+Per maggiori informazioni sulla visualizzazione dei dati e sulle opzioni di download, visita il nostro portale Open Data: https://opendata.tigregotico.pt
 
 Apprezziamo il tuo contributo e speriamo che tu ti unisca a noi nella creazione di un'esperienza software migliore per tutti!
 
-Accetti di condividere con noi dati anonimi?
+Accetti quindi di condividere con noi dati anonimi?
 "
 TITLE="Installazione Open Voice OS - Dati relativi all’utilizzo"
 
