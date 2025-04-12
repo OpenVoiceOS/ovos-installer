0b3f9e69dabbec1a798dcb9f85b8f0d585776f37
Denis Pitzalis <denics@free.fr>
2025-04-12 14:23:09 +0200

    added missing it-it file

---
+++ tui/locales/it-it/usage_telemetry.sh
@@ -0,0 +1,17 @@
+#!/bin/env bash
+
+CONTENT="
+Scegliendo di condividere i dati relativi al tuo utilizzo, contribuirai in modo significativo ai nostri sforzi continui per migliorare le prestazioni e l'esperienza utente del nostro software. I dati che fornirai rimarranno anonimi e saranno fondamentali per ottimizzare la corrispondenza delle intenzioni, identificare eventuali punti deboli del sistema e prendere decisioni informate sullo sviluppo futuro.
+
+In cambio della tua partecipazione, ti offriremo informazioni preziose sulle tendenze di utilizzo, le intenzioni non riconosciute e analisi personalizzate tramite un dashboard interattivo. Queste informazioni possono aiutarti a ottimizzare l'assistente per adattarlo meglio alle tue esigenze specifiche, garantendo un’esperienza utente più piacevole ed efficiente.
+
+Per maggiori informazioni sulla visualizzazione dei dati e sulle opzioni di download, visita il nostro portale Open Data: 
+https://opendata.tigregotico.pt
+
+Apprezziamo il tuo contributo e speriamo che tu ti unisca a noi nella creazione di un'esperienza software migliore per tutti!
+
+Accetti di condividere con noi dati anonimi?
+"
+TITLE="Installazione Open Voice OS - Dati relativi all’utilizzo"
+
+export CONTENT TITLE
