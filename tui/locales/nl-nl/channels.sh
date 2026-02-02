358d3ab9698ecb4e280d636189735b322589154a
Gaëtan Trellu <gaetan.trellu@gmail.com>
2026-02-02 18:14:09 -0500

    [tuning] Fixes and CodeRabbit recommendations

--- tui/locales/nl-nl/channels.sh
+++ tui/locales/nl-nl/channels.sh
@@ -9,7 +9,7 @@ Open Voice OS heeft drie verschillende versies:
 
 De stabiele versie van Open Voice OS is wat we de meeste gebruikers aanraden. Die is goed getest en heeft alle nieuwste functies en verbeteringen die betrouwbaar zijn bevonden.
 
-De 'test' versie van Open Voice OS is wat experimenteler. Hierin zitten nieuwe functies en verbeteringen die nog getest worden. Hij is grotendeels stabiel, maar kan wel wat foutjes of problemen bevatten.
+De testversie van Open Voice OS is wat experimenteler. Hierin zitten nieuwe functies en verbeteringen die nog getest worden. Hij is grotendeels stabiel, maar kan wel wat foutjes of problemen bevatten.
 
 De alpha-versie van Open Voice OS is de meest experimentele versie. Hierin zitten de nieuwste functies en veranderingen die nog in ontwikkeling zijn. Hij kan instabiel zijn en is niet aan te raden voor dagelijks gebruik.
 
