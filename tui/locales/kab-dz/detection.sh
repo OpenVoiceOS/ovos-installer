fee2b0764f548c28ff224caf990a8ee4d7e29661
ButterflyOfFire <mokainformatiqueit@gmail.com>
2026-08-29 09:43:54 +0000

    Update detection.sh

--- tui/locales/kab-dz/detection.sh
+++ tui/locales/kab-dz/detection.sh
@@ -14,4 +14,9 @@ Ttxil-k, ha-tan ddaw-a tilɣa-nni yettwafen.
 "
 TITLE="Asbeddi Open Voice OS - Yettwaf"
 
-export CONTENT TITLE
+HARDWARE_CONFIRMATION_TITLE="Asbeddi n Open Voice OS - Asenqed n warrum"
+HARDWARE_CONFIRMATION_MARK2_CONTENT="Yettwaf-d Raspberry Pi 4 akked ubeqqeḍ n umeslaw TAS5806.\n\nAya yezmer ad yili d Mycroft Mark II, maca kra n yiɣewwaren imezganen (HAT) zemren ad d-fkent assaɣ i icban wa.\n\nWarrum-a, d Mycroft Mark II s tidet?"
+HARDWARE_CONFIRMATION_DEVKIT_CONTENT="Yettwaf-d Raspberry Pi 4 akked yibeqqḍen TAS5806 d attiny1614.\n\nAya yezmer ad yili d Mycroft DevKit, maca kra n yiɣewwaren imezganen (HAT) zemren ad d-fkent assaɣ i icban wa.\n\nWarrum-a, d Mycroft DevKit s tidet?"
+HARDWARE_CONFIRMATION_GENERIC_NOTE="Fren Uhu akken ad tkemmleḍ s webrid amatu n Raspberry Pi."
+
+export CONTENT TITLE HARDWARE_CONFIRMATION_TITLE HARDWARE_CONFIRMATION_MARK2_CONTENT HARDWARE_CONFIRMATION_DEVKIT_CONTENT HARDWARE_CONFIRMATION_GENERIC_NOTE
